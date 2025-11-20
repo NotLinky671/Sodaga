31bb70d2ef51f66f326387baf419315f0675e56e
LinkyMoyi <notlinky671@gmail.com>
2025-11-20 22:02:00 +0800

    Second DrawLine

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,59 @@
 #include "tgaimage.h"
 
-void drawLine(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+#include <cmath>
+
+void DrawLine(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+  bool steep = std::abs(x0 - x1) < std::abs(y0 - y1);
+
+  // 检查线是不是太陡
+  if (steep) {
+    std::swap(x0, y0);
+    std::swap(x1, y1);
+    steep = true;
+  }
+
+  // 检查是不是从右往左画
+  if (x0 > x1) {
+    std::swap(x0, x1);
+    std::swap(y0, y1);
+  }
+
+  int dx = x1 - x0;
+  int dy = y1 - y0;
+
+  float derror = std::abs(dy / (float)dx);
+  float error = 0;
+
+  int y = y0;
   for (int x = x0; x <= x1; x++) {
-    // t 代表当前画到了百分之几
-    float t = (x - x0) / (float)(x1 - x0);
+    if (steep)
+      image.set(y, x, color);
+    else
+      image.set(x, y, color);
 
-    // 根据比例算出当前的 y 在哪里
-    int y = y0 + (y1 - y0) * t;
+    error += derror;
 
-    image.set(x, y, color);
+    if (error > 0.5) {
+      y += (y1 > y0 ? 1 : -1);
+      error -= 1.0;
+    }
   }
 }
 
 const TGAColor white = TGAColor{255, 255, 255, 255};
-const TGAColor red = TGAColor{0, 0, 255, 255};  // 注意：TGA颜色顺序是BGRA
+const TGAColor red = TGAColor{255, 0, 0, 255};
+const TGAColor green = TGAColor{0, 255, 0, 255};
+const TGAColor blue = TGAColor{0, 0, 255, 255};
 
 int main(int argc, char** argv) {
   TGAImage image(100, 100, TGAImage::RGB);
 
-  drawLine(13, 20, 80, 40, image, white);
+  // 画三条线测试一下
+  DrawLine(7, 3, 12, 37, image, red);
+  DrawLine(12, 37, 62, 53, image, green);
+  DrawLine(62, 53, 7, 3, image, blue);
 
-  image.flip_vertically();  // 这一行是为了让坐标原点在左下角（符合数学习惯）
+  image.flip_vertically();
   image.write_tga_file("output.tga");
-
   return 0;
 }
